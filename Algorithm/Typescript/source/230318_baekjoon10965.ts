6a66de0d5315dc3f2d4d0a8f2d2d3efb64566943
jihun90 <jihun90.kim@icloud.com>
2023-03-20 01:11:10 +0900

    ...

--- Algorithm/Typescript/source/230318_baekjoon10965.ts
+++ Algorithm/Typescript/source/230318_baekjoon10965.ts
@@ -1,8 +1,8 @@
 let inputPath: string = '';
-if (process.argv[2] = 'True') inputPath = process.argv[3];
-else inputPath = '/dev/stdin';
+inputPath = process.argv[2];
+//inputPath = '/dev/stdin';
 
 let input : string[] = require('fs').readFileSync(inputPath).toString().split('\n');
 
-let numbers = input[0].split(' ');
+let numbers : string[] = input[0].split(' ');
 console.log(Number(numbers[0]) * Number(numbers[1]));
