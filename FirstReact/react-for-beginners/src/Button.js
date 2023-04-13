e872bea5d923ef008ef594dfa77db975e864af60
jihun90 <jihun90.kim@icloud.com>
2023-04-13 21:59:46 +0900

    react app 연습

--- FirstReact/react-for-beginners/src/Button.js
+++ FirstReact/react-for-beginners/src/Button.js
@@ -1,5 +1,12 @@
-function Button({text}){
-    return <button>{text}</button>
+import PropTypes from "prop-types";
+import styled from "./Button.module.css";
+
+function Button({ text }) {
+  return <button className={styled.btn}>{text}</button>;
 }
+
+Button.propTypes = {
+  text: PropTypes.string.isRequired,
+};
 
 export default Button;
