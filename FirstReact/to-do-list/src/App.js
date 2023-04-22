efcbb269af0c91cd8d08027a1d7e91bb7b3c6d68
jihun90 <jihun90.kim@icloud.com>
2023-04-22 17:35:29 +0900

    to do list 생성(display는 안해줌)

---
+++ FirstReact/to-do-list/src/App.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, SetToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(toDo);
+    if (toDo === "") return;
+
+    SetToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
+  console.log(toDos);
+  return (
+    <div>
+      <h1>My To Dos ({toDosx.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do..." />
+        <button>Add To Do</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
