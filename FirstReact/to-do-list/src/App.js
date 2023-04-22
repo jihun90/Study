386ecc6f77e7de777e0b403ac36c66fc2e78f70f
jihun90 <jihun90.kim@icloud.com>
2023-04-22 23:03:03 +0900

    오타 지움

--- FirstReact/to-do-list/src/App.js
+++ FirstReact/to-do-list/src/App.js
@@ -15,7 +15,7 @@ function App() {
   console.log(toDos);
   return (
     <div>
-      <h1>My To Dos ({toDosx.length})</h1>
+      <h1>My To Dos ({toDos.length})</h1>
       <form onSubmit={onSubmit}>
         <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do..." />
         <button>Add To Do</button>
