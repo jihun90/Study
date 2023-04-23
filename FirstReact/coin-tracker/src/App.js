9ede71d44bdb6269a864e0c730c8140582b0a3e5
jihun90 <jihun90.kim@icloud.com>
2023-04-23 17:44:04 +0900

    bitcoin api를 이용해 여러 비트코인 가격 보여주기

--- FirstReact/coin-tracker/src/App.js
+++ FirstReact/coin-tracker/src/App.js
@@ -1,8 +1,29 @@
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
 
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>The Conins ({coins.length})</h1>
+      {loading ? <strong>Loding...</strong> : null}
+      <ul>
+        {coins.map((coin) => (
+          <li key={coin.id}>
+            {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
