bf8de6f857473c0d3313b73f311d614f39a70784
jihun90 <jihun90.kim@icloud.com>
2023-04-23 23:28:25 +0900

    달라를 입력하여 선택한 Coin 가격으로 계산해주는 기능 구현

--- FirstReact/coin-tracker/src/App.js
+++ FirstReact/coin-tracker/src/App.js
@@ -3,6 +3,9 @@ import { useEffect, useState } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [USD, setUSD] = useState();
+  const [coin, setCoin] = useState(0);
+  const [coinPerUSD, setCoinSelected] = useState();
 
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
@@ -10,25 +13,48 @@ function App() {
       .then((json) => {
         setCoins(json);
         setLoading(false);
+        setCoinSelected(json[0].quotes.USD.price);
       });
   }, []);
 
-  function CoinList() {
+  const onChangedUSD = (event) => {
+    setUSD(event.target.value);
+    setCoin(event.target.value * coinPerUSD);
+  };
+
+  const onChangeCoinSeleced = (event) => {
+    setCoinSelected(event.currentTarget.value);
+    setUSD(1.0);
+  };
+
+  function InputText({ value, onChanged }) {
     return (
-      <select>
-        {coins.map((coin) => (
-          <option key={coin.id}>
-            {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
-          </option>
-        ))}
-      </select>
+      <div>
+        <input value={value} onChange={onChanged} type="text" placeholder="Enter USD" />
+      </div>
+    );
+  }
+
+  function FormConvertUSDToCoin() {
+    return (
+      <div>
+        <select onChange={onChangeCoinSeleced}>
+          {coins.map((coin, index) => (
+            <option key={index} value={coin.quotes.USD.price}>
+              {coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+        <InputText value={USD} onChanged={onChangedUSD} />
+        <h2>you can need {coin} coin</h2>
+      </div>
     );
   }
 
   return (
     <div>
       <h1>The Conins {loading ? "" : `(${coins.length})`}</h1>
-      {loading ? <strong>Loding...</strong> : <CoinList />}
+      {loading ? <strong>Loding...</strong> : <FormConvertUSDToCoin />}
     </div>
   );
 }
