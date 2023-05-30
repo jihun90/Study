53b955272097230620de2b913f56e0be90a3cffb
jihun90 <jihun90.kim@icloud.com>
2023-05-31 06:59:44 +0900

    경로 잘못 설정된거 수정

---
+++ learn-typescript/why-ts/app.js
@@ -0,0 +1,49 @@
+// api url
+var url = "https://jsonplaceholder.typicode.com/users/1";
+
+// dom
+var username = document.querySelector("#username");
+var email = document.querySelector("#email");
+var address = document.querySelector("#address");
+
+// user data
+var user = {};
+
+/**
+ * @typedef {object} Address
+ * @property {string} street
+ * @property {string} city
+ */
+
+/**
+ * @typedef {object} User
+ * @property {string} name
+ * @property {string} email
+ * @property {Address} address
+ */
+
+/**
+ * @returns {Promise<User>}
+ */
+function fetchUser() {
+  return axios.get(url);
+}
+
+fetchUser().then(function (response) {
+  response.address;
+});
+
+function startApp() {
+  fetchUser()
+    .then(function (response) {
+      user = response.data;
+      username.innerText = user.name;
+      email.innerText = user.email;
+      address.innerText = user.address.street;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+startApp();
---
+++ learn-typescript/why-ts/sample.js
@@ -0,0 +1,18 @@
+// function sumUsingJS(a, b) {
+//   return a + b;
+// }
+
+// var resultUsingJS = sum(10, 20);
+// resultUsingJS.toLocaleString(); //js 자동완성 안됨.. ㅠㅠ
+
+// @ts-check
+
+/**
+ * @param {number} a 첫번째 숫자
+ * @param {number} b 두번째 숫자
+ */
+function sumUsingJS(a, b) {
+  return a + b;
+}
+
+// sum(10, "20");
