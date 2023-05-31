c6423981b778df0c3d50bdf3ced15071d5232f8d
jihun90 <jihun90.kim@icloud.com>
2023-06-01 07:20:36 +0900

    JS Prototype

--- learn-typescript/class-note/class.js
+++ learn-typescript/class-note/class.js
@@ -1,4 +1,11 @@
 // ES2015 (ES6)
+
+// function Person(name, age){
+//     this.name = name;
+//     this.age = age;
+// }
+// let kim = new Person('kim',1000); 
+
 class Person {
     // 클래스 로직
     constructor(name, age) {
